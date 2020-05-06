33e9a4e2f21ec0cf77080e460e52f708cafccd34
albertlav <albertlav@gmail.com>
2020-05-06 10:01:01 +0200

    Update DbgkCaptureLiveKernelDump.ps1

--- DbgkCaptureLiveKernelDump.ps1
+++ DbgkCaptureLiveKernelDump.ps1
@@ -1,17 +1,48 @@
 <#
-Creates Kernel Dump of live running Windows system without blue screen stop
+Creates Kernel Dump of live running Windows system without blue screen stop, using bare powershell
 Basically it tries to reproduce behavior of Sysinternals LiveKd.exe with -ml param
+lot of unknown params.. but it works
+Make sure you have SeDebugPrivilege
+result will go to %temp%
+#>
 
-1: kd> kv4
+$ntdll=Add-Type -MemberDefinition @"
+[StructLayout(LayoutKind.Sequential)]  
+public struct ObjectForNtSystemDebugControlSize64 {
+    public UInt32 Val0;              //??
+    public UInt32 Val1;              //used as bugcheck code
+    public UInt64 Val2;              //Val2 - Val5 used as bugcheck params
+    public UInt64 Val3;
+    public UInt64 Val4;
+    public UInt64 Val5;
+    public IntPtr FileHandle;        //handle to file
+    public IntPtr Val7;              //??
+    public UInt32 Val8;              //??
+    public UInt32 Val9;              //??
+};
+[DllImport("ntdll.dll")]
+public static extern UInt32 NtSystemDebugControl(UInt32 arg0, IntPtr arg1, UInt32 arg2, IntPtr arg3, UInt32 arg4, IntPtr arg5);
+"@ -Name 'ntdll' -Namespace 'api'
+
+$file=$(New-Item -ItemType File -Path "$env:TEMP\kernel-$($(get-date).ToFileTimeUtc()).dmp" -Force).OpenWrite()
+$o=New-Object -TypeName api.ntdll+ObjectForNtSystemDebugControlSize64
+$o.FileHandle=$file.Handle
+[IntPtr]$ptr = [System.Runtime.InteropServices.Marshal]::AllocHGlobal(0x40)
+[System.Runtime.InteropServices.Marshal]::StructureToPtr($o,$ptr,$false)
+[api.ntdll]::NtSystemDebugControl(0x25,$ptr,0x40,[System.IntPtr]::Zero,0,[System.IntPtr]::Zero)
+$file.Close()
+
+<#
+1: kd> kv3
  # Child-SP          RetAddr           : Args to Child                                                           : Call Site
 00 ffffb283`4bb07868 fffff801`57128532 : ffffb283`4bb078d0 fffff801`5748feaa fffff801`5707abe0 7fff980e`59e8cf00 : nt!IoCaptureLiveDump
 01 ffffb283`4bb07870 fffff801`577bffde : ffff980e`00000020 ffffffff`800023b4 ffff980e`593d4d60 ffff980e`59e8ce80 : nt!DbgkCaptureLiveKernelDump+0x2ca
 02 ffffb283`4bb07900 fffff801`57078e15 : 00000000`00000025 000000d1`b7cfddd0 00000000`00000040 00000000`00000000 : nt!NtSystemDebugControl+0x3de
-03 ffffb283`4bb07a10 00007fff`c711f794 : 00007ff6`e7e489c2 00007ff6`e7ea6400 000000d1`b7cfeb10 00000000`00000000 : nt!KiSystemServiceCopyEnd+0x25 (TrapFrame @ ffffb283`4bb07a80)
 
-00000000`00000025 
+
+00000000`00000025 <-
 000000d1`b7cfddd0 <-
-00000000`00000040  
+00000000`00000040 <-
 00000000`00000000
 
 1: kd> dd 000000d1`b7cfddd0 000000d1`b7cfddd0+0x40
@@ -37,32 +68,4 @@ Object: ffff980e59e8ce80  Type: (ffff980e502fad20) File
         HandleCount: 2  PointerCount: 65537
         Directory Object: 00000000  Name: \livekd.dmp {HarddiskVolume2}
 
-
-use with caution, lot of unknown params..
 #>
-
-$ntdll=Add-Type -MemberDefinition @"
-[StructLayout(LayoutKind.Sequential)]  
-public struct ObjectForNtSystemDebugControlSize64 {
-    public UInt32 Val0;              //??
-    public UInt32 Val1;              //used as bugcheck code(?)
-    public UInt64 Val2;              //Val2 - Val5 used as bugcheck params(?)
-    public UInt64 Val3;
-    public UInt64 Val4;
-    public UInt64 Val5;
-    public IntPtr FileHandle;        //handle to file
-    public IntPtr Val7;              //??
-    public UInt32 Val8;              //??
-    public UInt32 Val9;              //??
-};
-[DllImport("ntdll.dll")]
-public static extern UInt32 NtSystemDebugControl(UInt32 arg0, IntPtr arg1, UInt32 arg2, IntPtr arg3, UInt32 arg4, IntPtr arg5);
-"@ -Name 'ntdll' -Namespace 'api'
-
-$file=$(New-Item -ItemType File -Path "$env:TEMP\kernel-$($(get-date).ToFileTimeUtc()).dmp" -Force).OpenWrite()
-$o=New-Object -TypeName api.ntdll+ObjectForNtSystemDebugControlSize64
-$o.FileHandle=$file.Handle
-[IntPtr]$ptr = [System.Runtime.InteropServices.Marshal]::AllocHGlobal(0x40)
-[System.Runtime.InteropServices.Marshal]::StructureToPtr($o,$ptr,$false)
-[api.ntdll]::NtSystemDebugControl(0x25,$ptr,0x40,[System.IntPtr]::Zero,0,[System.IntPtr]::Zero)
-$file.Close()
