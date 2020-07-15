aa8da5723eca2ce1c13dbd6d06c45b283b25dc44
albertlav <albertlav@gmail.com>
2020-07-15 08:38:01 +0200

    Update ProcessDump.ps1
    changing _MINIDUMP_TYPE to more universal

--- ProcessDump.ps1
+++ ProcessDump.ps1
@@ -1,10 +1,11 @@
 #create usermode dump of process by PID
-#_MINIDUMP_TYPE=MiniDumpWithFullMemory (0x2)
+#4th arg _MINIDUMP_TYPE=MiniDumpWithFullMemory | MiniDumpWithFullMemoryInfo | MiniDumpWithHandleData | MiniDumpWithProcessThreadData| MiniDumpWithThreadInfo | MiniDumpWithUnloadedModules
+#modify for your needs using https://docs.microsoft.com/en-us/windows/win32/api/minidumpapiset/ne-minidumpapiset-minidump_type
 #result will go to %temp% with timestamp
 
 $pidToDump=26452 #give here processid to dump
 $process=$(Get-Process -Id $pidToDump)
 $file=$(New-Item -ItemType File -Path $env:TEMP\$pidTodump-$($(get-date).ToFileTimeUtc()).dmp -Force).OpenWrite()
 $dbghelp=Add-Type -MemberDefinition '[DllImport("dbghelp.dll")] public static extern bool MiniDumpWriteDump(IntPtr hProcess, UInt32 ProcessId, IntPtr hFile, UInt32 DumpType, IntPtr ExceptionParam, IntPtr UserStreamParam, IntPtr CallbackParam);' -Name 'dbghelp' -PassThru
-$dbghelp::MiniDumpWriteDump($process.Handle, $process.Id, $file.Handle, 2, [System.IntPtr]::Zero, [System.IntPtr]::Zero, [System.IntPtr]::Zero)
+$dbghelp::MiniDumpWriteDump($process.Handle, $process.Id, $file.Handle, 0x00000002 -bor 0x00000100 -bor 0x00001000 -bor 0x00000800 -bor 0x00000004 -bor 0x00000020, [System.IntPtr]::Zero, [System.IntPtr]::Zero, [System.IntPtr]::Zero)
 $file.Close()
